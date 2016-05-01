633052311ddce649e886bed543a838f96123692b
Mahmoud Adly Ezzat <mahmoud.adly@gmail.com>
2016-05-01 11:33:42 +0200

    Trim parsed text.

--- shared.php
+++ shared.php
@@ -4,10 +4,10 @@ function readDateFile() {
   $date = fread($date_file,filesize("hijri_date.txt"));
   fclose($date_file);
   $dates = explode(" ", $date);
-  
-  $day = $dates[0]; // first item is day
-  $year = array_pop($dates); // last item is year, pop it out
-  $month =  join(" ",array_slice($dates,1)); //then from second to last is the month
+
+  $day = trim($dates[0]); // first item is day
+  $year = trim(array_pop($dates)); // last item is year, pop it out
+  $month =  trim(join(" ",array_slice($dates,1))); //then from second to last is the month
 
   return array($day,$month,$year);
 }
