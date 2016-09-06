445e3a1c390e4199d7cee127f2a311f0345fc8b3
Mahmoud Adly <MahmoudAdly@users.noreply.github.com>
2016-09-06 15:58:49 +0200

    Update analytics code.

--- widget.php
+++ widget.php
@@ -90,7 +90,7 @@ else
       m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
       })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-      ga('create', 'UA-63683830-2', 'auto');
+      ga('create', 'UA-63683830-6', 'auto');
       ga('send', 'pageview');
 
     </script>
